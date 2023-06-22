d745eeb710dc6c61a29a1272f3e21ff489c34bb8
AntonDubovitskii <gtoll@yandex.ru>
2023-06-22 16:55:48 +0400

    Homework3 - Done

--- anton_framework/templator.py
+++ anton_framework/templator.py
@@ -1,18 +1,14 @@
-from jinja2 import Template
-from os.path import join
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
     """
     Функция, визуализирующая шаблон html-страницы.
-    :param template_name: имя шаблона
-    :param folder: директория, в которой находится шаблон
-    :param kwargs: параметры
-    :return: 
     """""
-    file_path = join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
 
 
